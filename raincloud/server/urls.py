1a9a6237ac806e846149eeef239d89d5db5a7cc9
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-07 10:41:49 -0800

    Added data post endpoint

--- raincloud/server/urls.py
+++ raincloud/server/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('data/<int:sensor_id>/', views.Data.as_view(), name='data')
 ]
--- raincloud/server/views.py
+++ raincloud/server/views.py
@@ -1,7 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+import json
 
-# Create your views here.
 
 def index(request):
     return HttpResponse("Hello raincloud!")
+
+
+class Data(View):
+
+    def post(self, request, sensor_id, *args, **kwargs):
+        print(f"received data from sensor id: {sensor_id}")
+        body = json.loads(request.body)
+        print(body)
+
+
+"""
+get sensor?query_params
+Default - return all 
+query_params:
+type=pressure
+units
+location
+
+https://stackoverflow.com/questions/150505/capturing-url-parameters-in-request-get
+
+
+get sensor/<sensor_id>
+
+
+post /data/<sensor_id>/
+    body:
+        {
+            timestamps: [
+                time,
+                time,
+                time
+            ],
+            values: [
+                value,
+                value, 
+                value
+            ]
+        }
+
+get /data/<sensor_id>?query_params
+    default: return last 12 hours of data
+    query_params:
+        start_time
+        end_time
+        sample_rate
+
+
+Sensor:
+- type (pressure, temperature, etc)
+- units
+- description
+- sample rate
+- measid
+- location
+
+"""
